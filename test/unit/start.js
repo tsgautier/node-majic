40934f5169f410f387eb376157c2731b32eb999e
Taylor Gautier <tgautier@gsngames.com>
2016-05-10 14:54:39 -0700

    better unit test naming

--- test/unit/start.js
+++ test/unit/start.js
@@ -1,4 +1,4 @@
-describe('options', () => {
+describe('start script', () => {
     it('should start with non null opts', () => {
         require('../../index.js').start({ verbose: false, scan: [] });
     });
